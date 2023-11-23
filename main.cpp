62be1f91650cdc8759b7a8d9386941c3ea50a87f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-23 21:47:46 +0700

    chore(main): update with declaration

--- main.cpp
+++ main.cpp
@@ -5,6 +5,62 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void pause();
+
+void clearScreen();
+
+float penjumlahan();
+
+float hitungLuasSegitiga();
+
+float hitungKelilingSegitiga();
+
+int main() {
+  int pilihan;
+  float hasil;
+
+  do {
+    cout << "Selamat datang di calculator segitiga" << endl;
+    cout << "====================================" << endl;
+    cout << "Pilih menu yang diinginkan: " << endl;
+    cout << "1. Hitung luas segitiga" << endl;
+    cout << "2. Hitung keliling segitiga" << endl;
+    cout << "3. Penjumlahan" << endl;
+    cout << "4. Keluar" << endl;
+    cout << "====================================" << endl;
+    cout << "Masukkan pilihan: ";
+    cin >> pilihan;
+
+    clearScreen();
+
+    switch (pilihan) {
+      case 1:
+        hasil = hitungLuasSegitiga();
+        cout << "Luas segitiga adalah: " << hasil << endl;
+        pause();
+        break;
+      case 2:
+        hasil = hitungKelilingSegitiga();
+        cout << "Keliling segitiga adalah: " << hasil << endl;
+        pause();
+        break;
+      case 3:
+        hasil = penjumlahan();
+        cout << "Hasil penjumlahan adalah: " << hasil << endl;
+        pause();
+        break;
+      case 4:
+        cout << "Terima kasih telah menggunakan calculator segitiga" << endl;
+        break;
+      default:
+        cout << "Pilihan tidak tersedia" << endl;
+        break;
+    }
+  } while (pilihan != 4);
+
+  return 0;
+}
+
 void clearScreen() {
   system("cls");
 }
@@ -48,50 +104,4 @@ float hitungKelilingSegitiga() {
   cin >> sisi3;
 
   return sisi1 + sisi2 + sisi3;
-}
-
-int main() {
-  int pilihan;
-  float hasil;
-
-  do {
-    cout << "Selamat datang di calculator segitiga" << endl;
-    cout << "====================================" << endl;
-    cout << "Pilih menu yang diinginkan: " << endl;
-    cout << "1. Hitung luas segitiga" << endl;
-    cout << "2. Hitung keliling segitiga" << endl;
-    cout << "3. Penjumlahan" << endl;
-    cout << "4. Keluar" << endl;
-    cout << "Masukkan pilihan: ";
-    cin >> pilihan;
-
-    switch (pilihan) {
-      case 1:
-        clearScreen();
-        hasil = hitungLuasSegitiga();
-        cout << "Luas segitiga adalah: " << hasil << endl;
-        pause();
-        break;
-      case 2:
-        clearScreen();
-        hasil = hitungKelilingSegitiga();
-        cout << "Keliling segitiga adalah: " << hasil << endl;
-        pause();
-        break;
-      case 3:
-        clearScreen();
-        hasil = penjumlahan();
-        cout << "Hasil penjumlahan adalah: " << hasil << endl;
-        pause();
-        break;
-      case 4:
-        cout << "Terima kasih telah menggunakan calculator segitiga" << endl;
-        break;
-      default:
-        cout << "Pilihan tidak tersedia" << endl;
-        break;
-    }
-  } while (pilihan != 4);
-
-  return 0;
 }
